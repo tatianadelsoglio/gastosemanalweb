6043d75871a67f3fcf0540200d30528d44c5bf4c
tatianadelsoglio <tatianadelsoglio96@gmail.com>
2021-08-13 13:56:51 -0300

    creacion primer state

--- src/componentes/pregunta.js
+++ src/componentes/pregunta.js
@@ -1,6 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const pregunta = () => {
+
+    //definir el state
+    const [cantidad, guardarCantidad] = useState(0);
+
+    //Función que lee el presupuesto. Como necesitamos acceder los valores, le pasamos e para tener acceso a e.target.value
+    // y e.target.name también.
+    const definirpresupuesto = e => {
+        //console.log(e.target.value); de esta manera puede comprobar si el valor numerico es tomado como string o como numero
+        
+
+    }
+/*Cuando en consola los números se muestran en color negro quiere decir que ese número en realidad es de tipo string,
+y cuando el número esta en color azul quiere decir que es de tipo entero*/
 
     return (
         <>
@@ -11,6 +24,7 @@ const pregunta = () => {
                 type= "number"
                 className= "u-full-width"
                 placeholder= "coloca tu presupuesto"
+                onChange={definirpresupuesto}
             />
 {/*El "u-full-width" lo que hace es que el elemento tome todo el espacio que hay disponible, esto viene de la estructura esqueleton (buscar)  */}
             <input
