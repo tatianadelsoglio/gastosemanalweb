ab95781fbeb5df912b58f7d7cce466a890631669
tatianadelsoglio <tatianadelsoglio96@gmail.com>
2021-08-17 09:28:57 -0300

    Modificacion en archivos preguntas.js y se creo el componente error.js

--- src/componentes/pregunta.js
+++ src/componentes/pregunta.js
@@ -1,25 +1,47 @@
 import React, { useState } from 'react';
+import Error from "./Error"
 
 const pregunta = () => {
 
     //definir el state
     const [cantidad, guardarCantidad] = useState(0);
 
+    //definir state para la validación
+    const [error, guardarError] = useState(false); // es falsa para indicar que no hay error al inicio
+
     //Función que lee el presupuesto. Como necesitamos acceder los valores, le pasamos e para tener acceso a e.target.value
     // y e.target.name también.
     const definirpresupuesto = e => {
         //console.log(e.target.value); de esta manera puede comprobar si el valor numerico es tomado como string o como numero
-        
+        guardarCantidad(parseInt(e.target.value, 10)) // con el parseInt() pasa a valores enteros lo que se le pase como argumento entre parentesis
 
     }
 /*Cuando en consola los números se muestran en color negro quiere decir que ese número en realidad es de tipo string,
 y cuando el número esta en color azul quiere decir que es de tipo entero*/
 
+    //Submit para definir el presupuesto
+    const agregarPresupuesto = e => {
+        e.preventDefault(); //Agregamos el preventDefault para que no lo envie el query string en la parte superior y tampoco recargue la página
+
+        //Validar
+        if (cantidad < 1 || isNaN( cantidad )) {
+            guardarError(true);
+            return;
+        }
+
+        //validación aprobada
+        guardarError(false);
+
+    }
+
+
     return (
         <>
             <h2>Colaca tu presupuesto</h2>
-
-            <form>
+            {error ? <Error mensaje="El Presupuesto es Incorrecto" />   : null}
+            <form
+                onSubmit = {agregarPresupuesto}
+            >
             <input
                 type= "number"
                 className= "u-full-width"
